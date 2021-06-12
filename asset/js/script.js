c95a6b4d121bd636534d672f8750007001017b78
junaiyet <junaiyetmia470@gmail.com>
2021-06-12 21:56:20 +0600

    asdf

---
+++ asset/js/script.js
@@ -0,0 +1,32 @@
+$(".bar").click(function(){
+    $(".side-ber").addClass("test")
+    $(".side-ber-tow").addClass("test2")
+})
+$(".close").click(function(){
+    $(".side-ber").removeClass("test")
+    $(".side-ber-tow").removeClass("test2")
+})
+$(".home").click(function(){
+    $(".drop").slideToggle()
+    $(".bar2").toggleClass("cls")
+})
+$(".teams").click(function(){
+    $(".drop2").slideToggle()
+       $(".bar3").toggleClass("cls")
+})
+$(".Services").click(function(){
+    $(".drop3").slideToggle()
+       $(".bar4").toggleClass("cls")
+})
+$(".Shops").click(function(){
+    $(".drop4").slideToggle()
+       $(".bar5").toggleClass("cls")
+})
+$(".Pages").click(function(){
+    $(".drop5").slideToggle()
+       $(".bar6").toggleClass("cls")
+})
+$(".Blog").click(function(){
+    $(".drop6").slideToggle()
+       $(".bar7").toggleClass("cls")
+})
